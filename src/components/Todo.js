11cd2d23e33529384122c130c2b4cf271de919f2
avinash-rj <avinash.r@iksula.com>
2023-03-21 14:45:19 +0530

    UI Complete

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,14 +3,26 @@ import logo from "../images/logo.png"
 const Todo = () => {
   return (
     <>
-      <div className='main-div'>
+      
         <div className='child-div'>
           <figure>
             <img src={logo} alt="todo" className='app-logo'/>
+            <h2>Add Your TODO List Here</h2>
           </figure>
+          <div className='addItems'>
+              <input type="text" placeholder='Add Items....'/>
+              <i class="fa fa-plus" title='Add Item'></i>
+          </div>
+          <div className='showItems'>
+              <div className='eachItems'>
+                  <h3>Apple</h3>
+              </div>
+              <i class="fa fa-trash" title='Delete Item'></i>
+          </div>
+         
+              <button className='removeAll'>Remove All</button>
+       
         </div>
-
-      </div>
     </>
   )
 }
